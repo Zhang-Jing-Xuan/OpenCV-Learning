b4847628d608d5fd6c32e993cae3cc354f29316f
Zhang-Jing-Xuan <70561649+Zhang-Jing-Xuan@users.noreply.github.com>
2021-01-17 13:10:44 +0800

    Add files via upload

---
+++ cv_Fourier.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import os
+os.chdir("/Users/admin/Desktop/CL/Python/OpenCV")
+img = cv2.imread('img4.jpg',0)
+
+img_float32 = np.float32(img)
+
+dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+# 得到灰度图能表示的形式
+magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
+#---------低通滤波--------
+dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+
+rows, cols = img.shape
+crow, ccol = int(rows/2) , int(cols/2)     # 中心位置
+
+# 低通滤波
+mask = np.zeros((rows, cols, 2), np.uint8)
+mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+
+# IDFT
+fshift = dft_shift*mask
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv2.idft(f_ishift)
+img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+plt.title('Result'), plt.xticks([]), plt.yticks([])
+plt.show()
+# 高通滤波
+mask = np.ones((rows, cols, 2), np.uint8)
+mask[crow-30:crow+30, ccol-30:ccol+30] = 0
+
+# IDFT
+fshift = dft_shift*mask
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv2.idft(f_ishift)
+img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+plt.title('Result'), plt.xticks([]), plt.yticks([])
+
+plt.show() 
